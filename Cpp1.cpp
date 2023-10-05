353dd5a32248568e9a772e1a7323bae454c27cfb
sagar <sagarbista098@gmail.com>
2023-10-05 20:23:38 +0545

    first commit

---
+++ Cpp1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    clrscr(); // Clear the screen (Windows-specific)
+    printf("Hello, World!\n");
+    printf("Press any key to exit...");
+    getch(); // Wait for a key press
+}
