657833ef964b45ec7b414f598cfaf9b92513dbb1
sagar <sagarbista098@gmail.com>
2023-10-05 20:57:18 +0545

    second

---
+++ Cpp2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+// cout Output stsream
+
+int main() {
+  cout << "Hellow world";
+  getch();
+
+    return 0;
+}
---
+++ Cpp3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+//
+{
+    int a;
+    cout<< "Enter the number:";
+    cin>> a;
+    cout << " Printing the value of a :"<<a;
+    getch();
+ return 0;
+}
